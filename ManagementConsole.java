96dd3a7552fcf36b4a7bfa05d5915e53bb7444e8
Nickola-Karparov <149435602+Nickola-Karparov@users.noreply.github.com>
2023-12-01 18:44:05 +0000

    Trying to add code files to main

---
+++ ManagementConsole.java
@@ -0,0 +1,29 @@
+package FrontEnd;
+
+import BackEnd.Library;
+
+import java.util.Scanner;
+
+public class ManagementConsole {
+    public static void main(String[] args) {
+        //Create an instance of the hub class
+        Library library=new Library();
+
+        //Create instance of console helper
+        ConsoleHelper helper=new ConsoleHelper(library);
+
+
+
+        helper.printMessage("Welcome to Library Manager 10.000! First let's create the library! \n" +
+                "First enter the number of floors the library has: ");
+        helper.createLibrary();
+       // helper.setNumFloors(floors=scanner.nextInt());
+       // System.out.println("Next, enter the number of bookshelves: ");
+       // helper.setNumShelves(shelves=scanner.nextInt());
+       // System.out.println("Finally, enter the current number of users: ");
+       // helper.setNumShelves(users=scanner.nextInt());
+
+//TODO: add create lib + options
+
+    }
+}
