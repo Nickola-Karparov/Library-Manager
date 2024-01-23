ffa89d2c10e16555a84969917cab985bffb1ee72
Nickola-Karparov <nikola.karparov.2020@gmail.com>
2024-01-23 13:05:19 +0000

    Revert "remove old inports"
    This reverts commit b7d0b69d0d8c5a0ebd8f16e11c71e48d7d466e73.

--- src/Book.java
+++ src/Book.java
@@ -55,6 +55,8 @@ public class Book {
         return this.Language;
     }
 
+    public void setLanguage(String language) {
+    }
 
     public String getTopic() {
         return this.Topic;
--- src/ConsoleHelper.java
+++ src/ConsoleHelper.java
@@ -53,41 +53,28 @@ public class ConsoleHelper {
         //set floor count
         setFloorCount();
 
-        createShelves();
-
-        createBooks();
-
-        //assign books to shelves
-        assignBooksToShelves();
-
-        createInitialUsers();
-
-    }
-
-    public void createShelves(){
         //set shelf count
         setShelfCount();
 
         //create and populate shelves
         createAndPopulateShelves();
-    }
-    public void createBooks(){
+
         //set books count
         setBooksCount();
 
         //create books
         createAndPopulateBooks();
 
-    }
+        //assign books to shelves
+        assignBooksToShelves();
 
-    public void createInitialUsers(){
         //set user count
         setUserCount();
 
         //create users
         createUsers();
-    }
 
+    }
 
     public void setFloorCount() {
         // Set the number of floors in the library
